3347ca35a60dd3295ce8c80e7089e241c7a120b2
helenngo733 <helenngo@csu.fullerton.edu>
2024-11-08 20:14:32 -0800

    updated for better testing

--- main.c
+++ main.c
@@ -17,11 +17,15 @@ void *handle_connection(void *client_socket) {
     char buffer[1024];
     int bytes_read;
 
+    printf("Handling Connection on %d\n", client_fd);
+
     // Echo back whatever the client sends
     while ((bytes_read = read(client_fd, buffer, sizeof(buffer))) > 0) {
         buffer[bytes_read] = '\0';
         printf("Received: %s\n", buffer);
         write(client_fd, buffer, bytes_read);
+
+        printf("Done with connection %d\n", client_fd);
     }
     close(client_fd);
     return NULL;
@@ -52,14 +56,16 @@ int main(int argc, char *argv[]) {
     return_value = bind(socket_fd, (struct sockaddr *)&socket_address, sizeof(socket_address));
     return_value = listen(socket_fd, LISTEN_BACKLOG);
 
+    struct sockaddr_in client_address;
+    socklen_t client_address_length = sizeof(client_address);
+
     // Accept connections and create threads to handle them
     while (1) {
-        struct sockaddr_in client_address;
-        socklen_t client_address_length = sizeof(client_address);
-
         int *client_fd = malloc(sizeof(int));
         *client_fd =
             accept(socket_fd, (struct sockaddr *)&client_address, &client_address_length);
+
+        printf("Accept connection on %d\n", *client_fd);
 
         pthread_t thread;
         pthread_create(&thread, NULL, handle_connection, client_fd);
